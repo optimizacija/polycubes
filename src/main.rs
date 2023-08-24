46855662aa089b9fe2409417a30d724d1d79b4e9
Jam <males.jan2@gmail.com>
2023-08-24 22:36:51 +0200

    feat: working solution - reflections counted as distinct

--- src/main.rs
+++ src/main.rs
@@ -7,61 +7,42 @@
 use std::fmt::{Formatter, Display, self};
 use std::hash::Hash;
 use std::collections::HashSet;
+use std::time::Instant;
 
 use log::{debug, info};
-
-static mut WIDTH: isize = 3;
-static mut HEIGHT: isize = 3;
-static mut DEPTH: isize = 3;
-
-fn step_dimensions() {
-    unsafe {
-        WIDTH += 2;
-        HEIGHT += 2;
-        DEPTH += 2;
-    }
-}
-
-fn width() -> isize {
-    unsafe { WIDTH }
-}
-
-fn height() -> isize {
-    unsafe { HEIGHT }
-}
-
-fn depth() -> isize{
-    unsafe { DEPTH }
-}
-
 
 #[derive(Clone, Hash, PartialEq, PartialOrd, Ord, Eq, Debug)]
 pub struct Bitfield3D {
     data: Vec<bool>,
+    width: isize,
+    height: isize,
+    depth: isize,
 }
 
-
 impl Bitfield3D {
-    pub fn new() -> Self {
+    pub fn new(width: isize, height: isize, depth: isize) -> Self {
         Self {
-            data: vec![false; (width() * height() * depth()) as usize],
+            data: vec![false; (width * height * depth) as usize],
+            width,
+            height,
+            depth,
         }
     }
 
     pub fn set_unchecked(&mut self, x: isize, y: isize, z: isize, value: bool) {
-        self.data[((x * height() + y) * depth() + z) as usize] = value;
+        self.data[((x * self.height + y) * self.depth + z) as usize] = value;
     }
 
 
     pub fn get_unchecked(&self, x: isize, y: isize, z: isize) -> bool {
-        self.data[((x * height() + y) * depth() + z) as usize]
+        self.data[((x * self.height + y) * self.depth + z) as usize]
     }
 
     fn touching_unset_bits(&self) -> impl Iterator<Item = (isize, isize, isize)> + '_ {
-        (0..width()).flat_map(move |x| {
-            (0..height()).flat_map(move |y| {
-                (0..depth()).filter_map(move |z| {
-                    if !self.get_unchecked(x, y, z) && self.has_set_neighbor(x, y, z) {
+        (-1..=self.width).flat_map(move |x| {
+            (-1..=self.height).flat_map(move |y| {
+                (-1..=self.depth).filter_map(move |z| {
+                    if (!self.is_inside(x, y, z) || !self.get_unchecked(x, y, z)) && self.has_set_neighbor(x, y, z) {
                         Some((x, y, z))
                     } else {
                         None
@@ -87,69 +68,69 @@ impl Bitfield3D {
     }
     
     fn is_inside(&self, x: isize, y: isize, z: isize) -> bool {
-        x >= 0 && x < width() && y >= 0 && y < height() && z >= 0 && z < depth()
+        x >= 0 && x < self.width && y >= 0 && y < self.height && z >= 0 && z < self.depth
     }
     
     fn index_unchecked(&self, x: isize, y: isize, z: isize) -> usize {
-        ((x * height()  + y) * depth() + z) as usize
+        ((x * self.height  + y) * self.depth + z) as usize
     }
 
     
     fn rotate_x(&self) -> Bitfield3D {
-        let mut new_data = vec![false; self.data.len()];
+        let mut result = Bitfield3D::new(self.width, self.depth, self.height);
 
-        for x in 0..width() {
-            for y in 0..height() {
-                for z in 0..depth() {
+        for x in 0..self.width {
+            for y in 0..self.height {
+                for z in 0..self.depth {
                     if self.get_unchecked(x, y, z) {
-                        let new_y = depth() - 1 - z;
+                        let new_y = self.depth - 1 - z;
                         let new_z = y;
-                        let index = self.index_unchecked(x, new_y, new_z);
-                        new_data[index] = true;
+                        let index = result.index_unchecked(x, new_y, new_z);
+                        result.data[index] = true;
                     }
                 }
             }
         }
 
-        Bitfield3D { data: new_data }
+        result
     }
 
     fn rotate_y(&self) -> Bitfield3D {
-        let mut new_data = vec![false; self.data.len()];
+        let mut result = Bitfield3D::new(self.depth, self.height, self.width);
 
-        for x in 0..width() {
-            for y in 0..height() {
-                for z in 0..depth() {
+        for x in 0..self.width {
+            for y in 0..self.height {
+                for z in 0..self.depth {
                     if self.get_unchecked(x, y, z) {
                         let new_x = z;
-                        let new_z = width() - 1 - x;
-                        let index = self.index_unchecked(new_x, y, new_z);
-                        new_data[index] = true;
+                        let new_z = self.width - 1 - x;
+                        let index = result.index_unchecked(new_x, y, new_z);
+                        result.data[index] = true;
                     }
                 }
             }
         }
 
-        Bitfield3D { data: new_data }
+        result
     }
 
     fn rotate_z(&self) -> Bitfield3D {
-        let mut new_data = vec![false; self.data.len()];
+        let mut result = Bitfield3D::new(self.height, self.width, self.depth);
 
-        for x in 0..width() {
-            for y in 0..height() {
-                for z in 0..depth() {
+        for x in 0..self.width {
+            for y in 0..self.height {
+                for z in 0..self.depth {
                     if self.get_unchecked(x, y, z) {
-                        let new_x = height() - 1 - y;
+                        let new_x = self.height - 1 - y;
                         let new_y = x;
-                        let index = self.index_unchecked(new_x, new_y, z);
-                        new_data[index] = true;
+                        let index = result.index_unchecked(new_x, new_y, z);
+                        result.data[index] = true;
                     }
                 }
             }
         }
 
-        Bitfield3D { data: new_data }
+        result
     }
     
     fn rotations(&self) -> impl Iterator<Item = Bitfield3D> + '_ {
@@ -187,41 +168,97 @@ impl Bitfield3D {
         })
     }
     
-    fn grow(&self) -> Bitfield3D {
+    fn grow_to_fit(&self, x: isize, y: isize, z: isize) -> Bitfield3D {
         // Increase the dimensions by 2 to account for padding on both sides
-        let new_width = width() + 2;
-        let new_height = height() + 2;
-        let new_depth = depth() + 2;
+        let offset_x = {
+            if x < 0 {
+                x.abs()
+            } else {
+                0
+            }
+        };
+        let offset_y = {
+            if y < 0 {
+                y.abs()
+            } else {
+                0
+            }
+        };
+        let offset_z = {
+            if z < 0 {
+                z.abs()
+            } else {
+                0
+            }
+        };
+        let new_width = {
+            if x >= self.width {
+                x + 1
+            } else {
+                self.width + offset_x
+            }
+        };
+        let new_height = {
+            if y >= self.height {
+                y + 1
+            } else {
+                self.height + offset_y
+            }
+        };
+        let new_depth = {
+            if z >= self.depth {
+                z + 1
+            } else {
+                self.depth + offset_z
+            }
+        };
 
         let mut new_data = vec![false; (new_width * new_height * new_depth) as usize];
 
-        for x in 0..width() {
-            for y in 0..height() {
-                for z in 0..depth() {
+        for x in 0..self.width {
+            for y in 0..self.height {
+                for z in 0..self.depth {
                     if self.get_unchecked(x, y, z) {
-                        // Translate to center the original data
-                        let new_x = x + 1;
-                        let new_y = y + 1;
-                        let new_z = z + 1;
-
+                        // takes offsets into account
+                        let index = (((x + offset_x) * new_height  + (y + offset_y)) * new_depth + (z + offset_z)) as usize;
                         
-                        let index = ((new_x * new_height  + new_y) * new_depth + new_z) as usize;
+                        // doesn't take into account offsets
+                        // let index = ((x * new_height  + y) * new_depth + z) as usize;
                         new_data[index] = true;
                     }
                 }
             }
         }
+        
+        debug!("len: {}, width: {}, height: {}, depth: {}", new_data.len(), new_width, new_height, new_depth);
 
-        Bitfield3D { data: new_data }
+        Bitfield3D { 
+            data: new_data,
+            width: new_width,
+            height: new_height,
+            depth: new_depth,
+        }
     }
 
     pub fn generate(&self, lookup: &mut HashSet<Bitfield3D>) -> Vec<Bitfield3D> {
-        let mut next = self.clone();
         self.touching_unset_bits()
-            .filter_map(|(x, y, z)| {
-                // debug!("self\n{}", self);
+            .filter_map(|(mut x, mut y, mut z)| {
+                let mut next = self.clone();
                 debug!("touching bit: ({}, {}, {})\n", x, y, z);
+                if !next.is_inside(x, y, z) {
+                    next = next.grow_to_fit(x, y, z);
+                    if x < 0 {
+                        x = 0;
+                    }
+                    if y < 0 {
+                        y = 0;
+                    }
+                    if z < 0 {
+                        z = 0;
+                    }
+                }
                 next.set_unchecked(x, y, z, true);
+                debug!("next\n{}", next);
 
                 // Use a scope to limit borrowing duration of self for cloning
                 let result = {
@@ -230,7 +267,7 @@ impl Bitfield3D {
                         debug!("canonical\n{}", canonical);
                          // TODO: 2 unnecessary clones, make lookup a hash of strings
                         lookup.insert(canonical.clone());
-                        Some(canonical.grow())
+                        Some(canonical.clone())
                     } else {
                         None
                     }
@@ -248,12 +285,12 @@ impl Bitfield3D {
 impl Display for Bitfield3D {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         write!(f, "ˇˇˇ\n")?;
-        for z in 0..depth() {
+        for z in 0..self.depth {
             if z != 0 {
                 write!(f, "---\n")?;
             }
-            for y in 0..height() {
-                for x in 0..width() {
+            for y in 0..self.height {
+                for x in 0..self.width {
                     let bit = if self.get_unchecked(x as isize, y as isize, z as isize) {
                         '1'
                     } else {
@@ -277,18 +314,17 @@ fn main() {
     
     // on first iteration, there is a single block
     curr_cc.push({
-        let mut first = Bitfield3D::new();
-        first.set_unchecked(1,1,1, true);
+        let mut first = Bitfield3D::new(1,1,1);
+        first.set_unchecked(0,0,0, true);
         first
     });
     
     for i in 1.. {
+        let start = Instant::now();
+        
         info!("{}: {}", i, curr_cc.len());
         for b in curr_cc.iter() {
             debug!("curr_cc\n{}", b);
-        }
-        if i == 3{
-            break;
         }
         
         for cc in curr_cc.iter() {
@@ -298,6 +334,8 @@ fn main() {
         std::mem::swap(&mut curr_cc, &mut next_cc);
         next_cc.clear();
         lookup.clear();
-        step_dimensions();
+        
+        let duration = start.elapsed();
+        info!("{}.{:03} seconds", duration.as_secs(), duration.subsec_nanos() / 1_000_000);
     }
 }
